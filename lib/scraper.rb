8a9715f4e41fccdc570f9ee7aba8edaa4d16d4f1
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-11 01:14:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,11 @@ class Scraper
       end
     end
   end
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
+  end
   
 end
 
